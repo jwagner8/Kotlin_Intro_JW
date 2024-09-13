f32423ca30d15daa681a18b74651f52e463bc204
Jensine Wagner <jensinewagner@10-252-176-98.student.trincoll.edu>
2024-09-13 12:22:14 -0400

    Initial commit

---
+++ src/main/java/Intro_Kotlin.kt
@@ -0,0 +1,62 @@
+import java.lang.IllegalArgumentException
+
+fun start(): String = "OK"
+
+fun joinOptions(options: Collection<String>) =
+    options.joinToString(prefix = "[", postfix = "]")
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+    (if (toUpperCase) name.uppercase() else name) + number
+
+fun useFoo() = listOf(
+    foo("a"),
+    foo("b", number = 1),
+    foo("c", toUpperCase = true),
+    foo(name = "d", number = 2, toUpperCase = true)
+)
+
+const val question = "life, the universe, and everything"
+const val answer = 42
+
+val tripleQuotedString = """
+    #question = "$question"
+    #answer = $answer""".trimMargin("#")
+
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+fun getPattern(): String = """\d{2} $month \d{4}"""
+
+fun sendMessageToClient(
+    client: Client?, message: String?, mailer: Mailer
+) {
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+
+
+fun failWithWrongAge(age: Int?): Nothing {
+    throw IllegalArgumentException("Wrong age: $age")
+}
+
+fun checkAge(age: Int?) {
+    if (age == null || age !in 0..150) failWithWrongAge(age)
+    println("Congrats! Next year you'll be ${age + 1}.")
+}
+
+fun containsEven(collection: Collection<Int>): Boolean =
+    collection.any { it % 2 == 0 }
+
+fun main(){
+    assert(start() == "OK")
+    println(tripleQuotedString)
+    checkAge(10)
+}
